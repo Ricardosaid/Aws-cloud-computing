db4e335c5b8ca37ae7169bad43e6a01ce5b56302
Ricardo Said Martinez <rsmartinez@bside.com.mx>
2022-11-24 14:02:51 -0600

    send one message

--- Amazon SQS/send_message.py
+++ Amazon SQS/send_message.py
@@ -8,8 +8,22 @@ queue = sqs.get_queue_by_name(QueueName = 'Test')
 
 #queue = sqs.create_queue(QueueName = 'Test', Attributes = {'DelaySeconds':'10'})
 
-print(queue.url)
-print(queue.attributes.get('DelaySeconds'))
+# Create a new message
+response = queue.send_message(
+    MessageBody="Hallo wie gehts!",
+    MessageAttributes = {
+        'Author' : {
+            'StringValue':'Saidleben',
+            'DataType':'String'
+        }
+    }
+)
+
+#The response is NOT a resource, but gives you a message ID
+print(response.get('MessageId'))
+print(response.get('MD50fMessageBody'))
+#print(queue.url)
+#print(queue.attributes.get('DelaySeconds'))
 
 
 
