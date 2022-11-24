311cf9dfd5414e8eb3c7217751de7c5ee83af6c0
Ricardo Said Martinez <rsmartinez@bside.com.mx>
2022-11-24 13:48:00 -0600

    get queue instance

--- Amazon SQS/send_message.py
+++ Amazon SQS/send_message.py
@@ -2,7 +2,11 @@ import boto3
 
 sqs = boto3.resource('sqs')
 
-queue = sqs.create_queue(QueueName = 'Test', Attributes = {'DelaySeconds':'10'})
+# Get an SQS.Queue instance
+
+queue = sqs.get_queue_by_name(QueueName = 'Test')
+
+#queue = sqs.create_queue(QueueName = 'Test', Attributes = {'DelaySeconds':'10'})
 
 print(queue.url)
 print(queue.attributes.get('DelaySeconds'))
